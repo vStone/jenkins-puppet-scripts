af603b3ed1e7efc749e68a0113a0743ac8489640
Jan Vansteenkiste <jan@vstone.eu>
2012-08-06 10:12:18 +0200

    Fix typo in common functions check_for_bash function

--- common/functions.sh
+++ common/functions.sh
@@ -4,7 +4,7 @@
 function check_for_bash() {
   pid=$$;
   if [[ -f "/proc/${pid}/cmdline" ]]; then
-    if [[ -z $( cat "/proc/${pid}/cmdline" | sed "s|$0||g" | grep "bash" ) ]    ]; then
+    if [[ -z $( cat "/proc/${pid}/cmdline" | sed "s|$0||g" | grep "bash" ) ]]; then
       return 1;
     else
       return 0;
