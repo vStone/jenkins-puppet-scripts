3a3fa4a66b5ed5bb0dd64d262ade8f74fab0b1ac
Jan Vansteenkiste <jan@vstone.eu>
2012-08-06 10:05:22 +0200

    Added a library for common functions

---
+++ common/functions.sh
@@ -0,0 +1,13 @@
+## check_for_bash
+#  Returns '0' if the script is running under bash
+#  Returns '1' if it doesn't.
+function check_for_bash() {
+  pid=$$;
+  if [[ -f "/proc/${pid}/cmdline" ]]; then
+    if [[ -z $( cat "/proc/${pid}/cmdline" | sed "s|$0||g" | grep "bash" ) ]    ]; then
+      return 1;
+    else
+      return 0;
+    fi;
+  fi;
+}
