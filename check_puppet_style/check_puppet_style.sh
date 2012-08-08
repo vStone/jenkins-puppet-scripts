8517e5acc9ce7d2d232e7b7afc43124fb7c511e2
Jan Vansteenkiste <jan@vstone.eu>
2012-08-08 12:37:43 +0200

    Rename the environment variables to resemble the purpose better and be more script specific (puppet-lint)

--- check_puppet_style/check_puppet_style.sh
+++ check_puppet_style/check_puppet_style.sh
@@ -19,25 +19,25 @@ Checks your puppet manifests with puppet-lint.
 
 OPTIONS:
   -t, --max-threads NUMER       The maximum number of simultaneous threads to
-                                use. You can also specify MAX_THREADS as an
+                                use. You can also specify PUPPET_LINT_THREADS as an
                                 environment variable. Defaults to 5.
   -p, --puppet-lint-bin PATH    Path to puppet-lint executable. Can also be
-                                specified as an env variable PUPPET_LINT.
+                                specified as an env variable PUPPET_LINT_BIN.
                                 Defaults to 'puppet-lint'.
   -l, --log-format              Override the log format. The default log format
                                 has been setup to use with the jenkins
                                 warnings-plugin (puppet-lint).
   -s, --skip-tests              Skips the tests directory in puppet modules.
-                                You can also specify SKIP_TESTS_DIR as an
+                                You can also specify PUPPET_LINT_SKIP_TESTS as an
                                 non empty environment variable.
   -e, --skip-examples           Skips the examples directory in puppet modules.
-                                You can also specify SKIP_EXAMPLES_DIR as an
+                                You can also specify PUPPET_LINT_SKIP_EXAMPLES as an
                                 non empty environment variable.
   -f, --fail-on-error           By default, this script always exits with an
                                 exit status of 0. If this is enabled, we will
                                 exit with a status of 1 if any errors are
                                 detected by puppet lint. You can also specify
-                                a non empty environment variable FAIL_ON_ERROR.
+                                a non empty environment variable PUPPET_LINT_FAILS_ERROR.
   -w, --fail-on-warning         Exit with a status of 1 if a warning is
                                 detected by puppet lint. This implies
                                 fail-on-error too. You can also specify a non
@@ -81,15 +81,15 @@ while [ $# -gt 0 ]; do
   case "$1" in
     -t|--max-threads)         echo "$2" | grep -q '^[0-9]\+$' || \
                                 syserr "max-threads should be a number";
-                              MAX_THREADS="$2"; shift;;
+                              PUPPET_LINT_THREADS="$2"; shift;;
     -p|--puppet-lint-bin)     [ -f $2 ] || \
                                 syserr "puppet-lint-bin: file does not exist.";
-                              PUPPET_LINT="$2"; shift;;
-    -l|--log-format)          LOG_FORMAT="$2"; shift;;
-    -s|--skip-tests)          SKIP_TESTS_DIR="1";;
-    -e|--skip-examples)       SKIP_EXAMPLES_DIR="1";;
-    -f|--fail-on-error)       FAIL_ON_ERROR="1";;
-    -w|--fail-on-warning)     FAIL_ON_WARNING="1";;
+                              PUPPET_LINT_BIN="$2"; shift;;
+    -l|--log-format)          PUPPET_LINT_LOG_FORMAT="$2"; shift;;
+    -s|--skip-tests)          PUPPET_LINT_SKIP_TESTS="1";;
+    -e|--skip-examples)       PUPPET_LINT_SKIP_EXAMPLES="1";;
+    -f|--fail-on-error)       PUPPET_LINT_FAILS_ERROR="1";;
+    -w|--fail-on-warning)     PUPPET_LINT_FAILS_WARNING="1";;
     -h|--help)                _help;;
     -*)                       syserr "Command option '$1' not recognized";;
     --)                       shift; break;;
@@ -102,38 +102,38 @@ if [ $# == 0 ]; then
   _help;
 fi
 
-MAX_THREADS="${MAX_THREADS-5}";
-SKIP_TESTS_DIR="${SKIP_TESTS_DIR}";
-SKIP_EXAMPLES_DIR="${SKIP_EXAMPLES_DIR}";
-PUPPET_LINT="${PUPPET_LINT-puppet-lint}";
-LOG_FORMAT="${LOG_FORMAT}"
-[ ! "${LOG_FORMAT}" ] && LOG_FORMAT="%{path}:%{linenumber}:%{check}:%{KIND}:%{message}"
+PUPPET_LINT_THREADS="${PUPPET_LINT_THREADS-5}";
+PUPPET_LINT_SKIP_TESTS="${PUPPET_LINT_SKIP_TESTS}";
+PUPPET_LINT_SKIP_EXAMPLES="${PUPPET_LINT_SKIP_EXAMPLES}";
+PUPPET_LINT_BIN="${PUPPET_LINT_BIN-puppet-lint}";
+PUPPET_LINT_LOG_FORMAT="${PUPPET_LINT_LOG_FORMAT}"
+[ ! "${PUPPET_LINT_LOG_FORMAT}" ] && PUPPET_LINT_LOG_FORMAT="%{path}:%{linenumber}:%{check}:%{KIND}:%{message}"
 
-FAIL_ON_WARNING="${FAIL_ON_WARNING}";
-FAIL_ON_ERROR="${FAIL_ON_ERROR-${FAIL_ON_WARNING}}";
+PUPPET_LINT_FAILS_WARNING="${PUPPET_LINT_FAILS_WARNING}";
+PUPPET_LINT_FAILS_ERROR="${PUPPET_LINT_FAILS_ERROR-${PUPPET_LINT_FAILS_WARNING}}";
 
-echo "Checking puppet style (Using $MAX_THREADS threads):"
+echo "Checking puppet style (Using $PUPPET_LINT_THREADS threads):"
 
 _find="find $* -iname '*.pp'"
-if [ "$SKIP_TESTS_DIR" ]; then
+if [ "$PUPPET_LINT_SKIP_TESTS" ]; then
   _find="${_find} ! -iwholename '*/tests/*'"
 fi;
-if [ "$SKIP_EXAMPLES_DIR" ]; then
+if [ "$PUPPET_LINT_SKIP_EXAMPLES" ]; then
   _find="${_find} ! -iwholename '*/examples/*'"
 fi;
 
 warning_count=0
 error_count=0
 
-eval $_find | xargs --no-run-if-empty -n1 -P${MAX_THREADS} \
-  $PUPPET_LINT --log-format "${LOG_FORMAT}" | (
+eval $_find | xargs --no-run-if-empty -n1 -P${PUPPET_LINT_THREADS} \
+  $PUPPET_LINT_BIN --log-format "${PUPPET_LINT_LOG_FORMAT}" | (
   while read line; do
     echo $line | grep -q ':WARNING:' && let "warning_count++"
     echo $line | grep -q ':ERROR:' && let "error_count++"
     echo $line
   done
-  [[ "$FAIL_ON_ERROR" && $error_count -gt 0 ]] && exit 1;
-  [[ "$FAIL_ON_WARNING" && $warning_count -gt 0 ]] && exit 1;
+  [[ "$PUPPET_LINT_FAILS_ERROR" && $error_count -gt 0 ]] && exit 1;
+  [[ "$PUPPET_LINT_FAILS_WARNING" && $warning_count -gt 0 ]] && exit 1;
   exit 0;
 ) || exit $?
 
