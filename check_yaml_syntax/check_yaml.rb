97eface8687ae7991dfccca6c1ed5b05027d525c
Jan Vansteenkiste <jan@vstone.eu>
2013-09-17 14:42:08 +0200

    Added initial script to check yaml syntax

--- check_yaml_syntax/check_yaml.rb
+++ check_yaml_syntax/check_yaml.rb
@@ -2,19 +2,36 @@
 require 'yaml'
 
 status=0
-ARGV.each do |file|
+
+def check_file(filename)
+  status=0
   begin
-    r = YAML.load_file(file)
+    r = YAML.load_file(filename)
   rescue Errno::ENOENT => ex
-    $stderr.print "#{file}:ERROR:0:File does not exist!\n"
-    status=2
+    $stderr.print "#{filename}:ERROR:0:File does not exist!\n"
+    status=1
   rescue Psych::SyntaxError => ex
-    $stderr.print "#{file}:ERROR:#{ex.line}:#{ex.problem}\n"
+    $stderr.print "#{filename}:ERROR:#{ex.line}:#{ex.problem}\n"
     status=1
   rescue Exception => ex
     $stderr.print "Something else went wrong"
   else
     ## Print OK?
+  end
+  status
+end
+
+ARGV.each do |file|
+  if File.directory?(file)
+    $stdout.print "#{file}:INFO:Is a directory. Scanning for *.yaml\n"
+    Dir.glob("#{file}/**/*.yaml").each do |f|
+      s = check_file(f)
+      status = s unless s == 0
+
+    end
+  else
+    s = check_file(file)
+    status = s unless s == 0
   end
 end
 
