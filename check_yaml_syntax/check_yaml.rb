16d50dff284699e0a20d01a3d2f62f0c575b95e7
Jan Vansteenkiste <jan@vstone.eu>
2013-09-26 14:45:33 +0200

    Only check for existance of hash if there are no parse errors

--- check_yaml_syntax/check_yaml.rb
+++ check_yaml_syntax/check_yaml.rb
@@ -18,7 +18,7 @@ def check_file(filename)
   else
     ## Print OK?
   end
-  unless r.is_a?(Hash)
+  unless status == 1 or r.is_a?(Hash)
     $stderr.print "YAML_SYNTAX:#{filename}:ERROR:0:YAML file does not contain a hash\n"
     status=1
   end
