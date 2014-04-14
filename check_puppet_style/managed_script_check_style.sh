2528326d07ede42277aaa9f7c6d4897e66334dd2
Jan Vansteenkiste <jan@vstone.eu>
2014-04-14 18:10:25 +0200

    Fix style of the script that is checking style. o sweet irony

--- check_puppet_style/managed_script_check_style.sh
+++ check_puppet_style/managed_script_check_style.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#        
-# Script wich catches the modified files from the latest commit in a git repository and     
+#
+# Script wich catches the modified files from the latest commit in a git repository and
 # runs a puppet-lint check on them
 #
-# Variables to be set: 
+# Variables to be set:
 #
 # PUPPET_SUBDIR
 #
-# PUPPET_LINT_THREADS, PUPPET_LINT_SKIP_TESTS, PUPPET_LINT_SKIP_EXAMPLES, 
+# PUPPET_LINT_THREADS, PUPPET_LINT_SKIP_TESTS, PUPPET_LINT_SKIP_EXAMPLES,
 # PUPPET_LINT_BIN, PUPPET_LINT_FAILS_WARNING, PUPPET_LINT_FAILS_ERROR
 #
 # scripts_job_name: Name of the jenkins job which is used to pull this repo into your jenkins environment
@@ -24,18 +24,18 @@ scripts_job_name="scripts/puppet"
 
 # Catch the modified .pp manifests, puts them in an array and use that array to peform the puppet-style checks
 declare -a files
-         
+
 for FILE in $(git diff --name-only --diff-filter=ACMRTUXB ${GIT_PREVIOUS_COMMIT} | grep ".pp$");
-do       
+do
 	files=("${files[@]}" $FILE)
-done     
-         
+done
+
 if [ ${#files[@]} -eq 0 ];then
 	echo "No modified manifests to check"
 else
 	for i in ${files[@]};
-	do       
-        	echo "Stylecheck on manifest $i:";                                                                                                       
+	do
+	  echo "Stylecheck on manifest $i:";
 		bash -e /var/lib/jenkins/$scripts_job_name/check_puppet_style/check_puppet_style.sh -x "${manifests_exclude}" $i || manifests_failed=1
 	done
 fi
@@ -44,16 +44,16 @@ fi
 declare -a modules
 
 for MODULE in $(git diff --name-only --diff-filter=ACMRTUXB ${GIT_PREVIOUS_COMMIT} | grep "^modules/");
-do       
+do
 	modules=("${modules[@]}" $MODULE)
-done     
-        
+done
+
 if [ ${#modules[@]} -eq 0 ];then
 	echo "No modified modules to check"
 else
 	for i in ${modules[@]};
-	do       
-        	echo "Stylecheck on module $i:";                                                                                                       
+	do
+    echo "Stylecheck on module $i:";
 		bash -e /var/lib/jenkins/$scripts_job_name/check_puppet_style/check_puppet_style.sh -x "${module_exclude}" $i || module_failed=1
 	done
 fi
