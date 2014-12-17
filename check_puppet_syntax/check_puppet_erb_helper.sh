c8678aa8bd5a817c86bf9be69765a18f7ac71da6
Thomas Kräuter <kraeutert@gmail.com>
2014-12-17 14:01:51 +0100

    Update check_puppet_erb_helper.sh
    disregard the lin which starts in "%"

--- check_puppet_syntax/check_puppet_erb_helper.sh
+++ check_puppet_syntax/check_puppet_erb_helper.sh
@@ -6,7 +6,7 @@ if [ ! -f "$f" ]; then
   exit 1;
 fi
 
-$ERB_BIN -x -S 0 -T '-' $f | $RUBY_BIN -c 2>&1 | while read line; do
+$ERB_BIN -P -x -S 0 -T '-' $f | $RUBY_BIN -c 2>&1 | while read line; do
   if echo "$line" | grep -q 'Syntax OK'; then
     echo "$f: $(tput setaf 2; tput bold)$line$( tput sgr0)";
   else
